769a6b95763243629ef7a61c56ebe0f88e3e435e
Yosbi Alves Saenz <yosbisaenz@icloud.com>
2023-10-26 21:14:54 +0100

    Initial Commit

---
+++ Pipeline/Pipeline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pipeline
+//
+//  Created by Yosbi Alves Saenz on 26/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pipeline/Pipeline/PipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineApp.swift
+//  Pipeline
+//
+//  Created by Yosbi Alves Saenz on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
