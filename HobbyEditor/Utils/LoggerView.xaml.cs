32b33822f94af797a37777cb3b543863338cad5a
Yosbi Alves Saenz <80166570+Yosbi@users.noreply.github.com>
2023-12-01 21:19:57 +0000

    Worked on the log control to do filters and clear and a little on the visualization of the messages

--- HobbyEditor/Utils/LoggerView.xaml.cs
+++ HobbyEditor/Utils/LoggerView.xaml.cs
@@ -24,5 +24,23 @@ namespace HobbyEditor.Utils
         {
             InitializeComponent();
         }
+
+        private void _onClearButtonClick(object sender, RoutedEventArgs e)
+        {
+            Logger.Clear();
+        }
+
+        private void _onMessageFilterButtonClick(object sender, RoutedEventArgs e)
+        {
+            var filter = 0x0;
+            if (toggleInfos.IsChecked == true)
+                filter |= (int)MessageType.Info;
+            if (toggleWarnings.IsChecked == true)
+                filter |= (int)MessageType.Warning;
+            if (toggleErrors.IsChecked == true)
+                filter |= (int)MessageType.Error;
+
+            Logger.SetFilter(filter);
+        }
     }
 }
